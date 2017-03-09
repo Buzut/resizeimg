7d6a26617174483b7985dd6c0c11513134cbc660
Buzut <blog@buzut.fr>
2017-03-09 19:27:00 +0100

    added files

---
+++ README.md
@@ -0,0 +1,77 @@
+# resize-image
+
+Resize image allows to efficiently resize images in the browser prior to upload thanks to HTML5 canvas.
+
+This module allows to resize files, thus making them smaller. Why wait for the file to be on the server before compressing it?
+
+## How does it work?
+
+Modern browsers have everything needed to resize images. That works in three steps:
+
+1. take an image object (from input file) as input param
+2. convert it into base64 dataUrl
+3. "print" that image on a canvas of desired size (canvas isn't inserted in the DOM at all)
+4. return the new image as base64 dataUrl
+
+
+## Installation
+
+### Bower
+
+```
+bower install compress-image
+```
+
+### Manual
+
+Download the archive and extract it. Then just link `resize-image.js` in your `<head>`:
+
+    <script src="resize-image.js"></script>
+
+## Can I haz the code?
+
+__Yes sir!__
+
+_You'll notice that this is ES6 code (so is the module, more details below), treat with caution._
+
+```js
+// get your input file (it has id="upload-image")
+const inputFile = document.getElementById('upload-image');
+
+// get the actual file object from your input field as soon as a file is selected
+inputFile.onchange = function(event) {
+   const img = inputFile.files[0];
+
+	resizeImage(file, { outputFormat: 'jpeg', targetWidth: 200, targetHeight: 100 }, (err, b64img) => {
+    	if (err) {
+    	    console.error(err);
+    	    return;
+    	}
+
+    	// do what you have to with the b64img
+	});
+});
+```
+
+### Parameters
+
+The function takes three arguments:
+
+- the image as a file object from input file
+- options object
+- callback
+
+The options object takes several arguments:
+
+- {string} outputFormat – (jpe?g or png)
+- {string} targetWidth
+- {string} targetHeight
+- {bool} forceRatio – if true, force dimensions without regard to the aspect ratio (optional)
+
+
+
+    
+
+## ES6 comatibility without a build pipeline
+
+The module is written is ES2015. If you want it compatible with (moderately) older browsers and don't have a build process, you'll have to [transpile it to ES5](https://es6console.com/). While you're at it, don't forget to uglify it for better delivery.
---
+++ resize-image.js
@@ -0,0 +1,88 @@
+window.resizeImage = (() => {
+    /**
+     * Take file object from input file and convert it to base64
+     * @param {object} file
+     * @callback – base64 dataUrl
+     */
+    function convertFileToB64(file, callback) {
+        const reader = new FileReader();
+
+        reader.addEventListener('load', () => {
+            callback(reader.result);
+        }, false);
+
+        reader.readAsDataURL(file);
+    }
+
+    /**
+     * Create an HTMLImageElement from b64 string
+     * @param {string} b64img
+     * @callback – HTMLImageElement
+     */
+    function getImage(b64img, callback) {
+        const imgObj = new Image();
+
+        imgObj.onload = () => {
+            callback(imgObj);
+        };
+
+        imgObj.src = b64img;
+    }
+
+    /**
+     * Resize an image, preserving (or not), its aspect ratio
+     * @param {object} file – file object from input file
+     * @param {object} options - this object requires the following options:
+     *  {string} outputFormat – (jpe?g or png)
+     *  {string} targetWidth
+     *  {string} targetHeight
+     *  {bool} forceRatio – if true, force dimensions without regard to the aspect ratio (optional)
+     * @callback – err, b64img (the image is returned as a base64 dataUrl string)
+     */
+    function resize(img, options, callback) {
+        if (!/(jpe?g|png)$/i.test(img.type)) {
+            callback(new TypeError('image must be either jpeg or png'));
+        }
+
+        if (options.outputFormat !== 'jpg' && options.outputFormat !== 'jpeg' && options.outputFormat !== 'png') {
+            callback(new Error('outputFormat must be either jpe?g or png'));
+        }
+
+        const output = (options.outputFormat === 'png') ? 'png' : 'jpeg';
+
+        convertFileToB64(img, (b64img) => {
+            getImage(b64img, (imgObj) => {
+                const canvas = document.createElement('canvas');
+                const context = canvas.getContext('2d');
+                const imgWidth = imgObj.width;
+                const imgHeight = imgObj.height;
+                let width;
+                let height;
+
+                if (!options.forceRatio) {
+                    if (imgWidth > imgHeight) {
+                        width = options.targetWidth;
+                        height = Math.round((imgHeight / imgWidth) * width);
+                    }
+
+                    else {
+                        height = options.targetHeight;
+                        width = Math.round((imgWidth / imgHeight) * height);
+                    }
+                }
+
+                else {
+                    width = options.targetWidth;
+                    height = options.targetHeight;
+                }
+
+                canvas.width = width;
+                canvas.height = height;
+                context.drawImage(imgObj, 0, 0, width, height);
+                callback(null, canvas.toDataURL(`image/${output}`));
+            });
+        });
+    }
+
+    return resize;
+})();
